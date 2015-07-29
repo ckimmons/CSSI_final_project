80ca1d56db2cec4388de8aa941d764c2a04dc280
GChamberlin <GChamberlin@users.noreply.github.com>
2015-07-29 10:25:14 -0700

    Update hoverShow.js

--- hoverShow.js
+++ hoverShow.js
@@ -1,7 +1,4 @@
 $(document).ready(function () {
-    $(document).on('mouseenter', '.mainitemlist', function () {
-        $(this).find("#englishSubList").show();
-    }).on('mouseleave', '.mainitemlist', function () {
-        $(this).find("#englishSubList").hide();
-    });
+    $('.hexagon').mouseenter(function(){
+        $(".SubList").show();
 });
