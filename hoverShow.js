4cc904b749eb729504a9094a040a2477efcaf49d
GChamberlin <GChamberlin@users.noreply.github.com>
2015-07-29 09:46:23 -0700

    Update hoverShow.js

--- hoverShow.js
+++ hoverShow.js
@@ -1,3 +1,7 @@
-$("#English").mouseenter(function(){
-    $("#englishSubList").toggle();
+$(document).ready(function () {
+    $(document).on('mouseenter', '.mainitemlist', function () {
+        $(this).find("#englishSubList").show();
+    }).on('mouseleave', '.mainitemlist', function () {
+        $(this).find("#englishSubList").hide();
+    });
 });
